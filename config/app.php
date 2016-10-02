a16f908a5489b72f35f87da4130ade19f54b6135
oz <first@xakep.ru>
2016-10-03 00:21:37 +0300

    02.10.2016 second commit

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
